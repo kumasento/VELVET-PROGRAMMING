7237f0ca16357cd791316cdf295fa699fbca6829
NaN3 <rz940303@gmail.com>
2014-04-19 09:33:08 +0800

    Finished SICP 4.2 Homework

---
+++ SICP/hw6/3.5_Monte_Carlo.rkt
@@ -0,0 +1,25 @@
+#lang planet neil/sicp
+
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (Pxy x1 x2 y1 y2)
+      (lambda () 
+        (let ((dx (- (random-in-range x1 x2) (/ (+ x1 x2) 2)))
+              (dy (- (random-in-range y1 y2) (/ (+ y1 y2) 2))))
+          (<= (+ (* dx dx) (* dy dy)) 1))))
+
+(define (monte-carlo trails experiment)
+  (define (iter trails-remaining trails-passed)
+    (cond ((= trails-remaining 0) (/ trails-passed trails))
+          ((experiment) (iter (- trails-remaining 1) (+ trails-passed 1)))
+          (else (iter (- trails-remaining 1) trails-passed))))
+  (iter trails 0))
+(define (experiment-integral Pxy x1 x2 y1 y2 trails)
+  (define square
+    (* (abs (- x1 x2)) (abs (- y1 y2))))
+  (* square (monte-carlo trails (Pxy x1 x2 y1 y2))))
+                                
+(display (experiment-integral Pxy 300 900 100 400 100000))
+    
