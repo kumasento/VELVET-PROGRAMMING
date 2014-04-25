2512c00fdfb80ca8fe9a6ca510d2c7793c822775
Vincent Zhao <rz940303@gmail.com>
2014-04-25 21:57:55 +0800

    2014-04-25 SICP bhw1 Step1: GN finished

--- SICP/bhw1-generic-arithmetic-package/main_procedure.rkt
+++ SICP/bhw1-generic-arithmetic-package/main_procedure.rkt
@@ -1,5 +1,16 @@
-#lang planet neil/sicp
-
-(load "types.scm")
+;;; put-get -- functions to manage the table
 (load "put-get.scm")
-(load "ps5-code.scm")
+;;;types -- apply-generic and type tagging
+(load "types.scm")
+
+(load "ps5-code-revised.scm")
+
+;Lab Exercise 5.1B
+(define num1 (create-number 1))
+(define num2 (create-number 2))
+(define num1-copy (create-number 1))
+
+(define (equ? x y) (apply-generic 'equ? x y))
+
+(equ? num1 num2) ;; should be false
+(equ? num1 num1-copy) ;; should be true
